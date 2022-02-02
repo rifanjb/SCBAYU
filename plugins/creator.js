ee049a60de16190f35b930a81ea84300c75b9ef7
Rifanshop <98645048+Rifanshop@users.noreply.github.com>
2022-02-02 08:15:10 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
   // F this.sendContact(m.chat, '6283172195617', 'Nurutomo', m)
-  this.sendContact(m.chat, '6283172195617', 'Bang rifan,' m)
+  this.sendContact(m.chat, '6283172195617', 'Bang rifan', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
